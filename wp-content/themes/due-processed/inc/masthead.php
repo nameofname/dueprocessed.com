2b7cbd97423f40f0f3f2c2e58794bf1bb2141ef6
Abraham Peterkin <abraham.peterkin@1stdibs.com>
2020-12-17 13:19:14 -0500

    fix syntax error

--- wp-content/themes/due-processed/inc/masthead.php
+++ wp-content/themes/due-processed/inc/masthead.php
@@ -4,7 +4,6 @@ use Carbon_Fields\Field;
 
 function crb_attach_post_meta() {
 	Container::make( 'post_meta', __( 'Page Options' ) )
-		>where( 'post_template', '=', 'page-about.php' )
 		->add_fields( array(
 			Field::make( 'complex', 'authors', 'Authors' )
 				->set_layout( 'tabbed-horizontal' )
