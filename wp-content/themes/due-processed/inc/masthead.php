e61d7843cf3a5fc059937407109a39082a6290a0
Abraham Peterkin <abraham.peterkin@1stdibs.com>
2020-12-17 13:02:27 -0500

    commit wp-content change

--- wp-content/themes/due-processed/inc/masthead.php
+++ wp-content/themes/due-processed/inc/masthead.php
@@ -4,7 +4,7 @@ use Carbon_Fields\Field;
 
 function crb_attach_post_meta() {
 	Container::make( 'post_meta', __( 'Page Options' ) )
-		->where( 'post_id', '=', '7' ) // 7 is the id of the about page
+		>where( 'post_template', '=', 'page-about.php' )
 		->add_fields( array(
 			Field::make( 'complex', 'authors', 'Authors' )
 				->set_layout( 'tabbed-horizontal' )
