ee5c3fccf6ed457bd4f0fa0784268bd01043db36
Abraham Peterkin <abraham.peterkin@1stdibs.com>
2020-12-16 22:24:08 -0500

    add author page and new profile fields

--- wp-content/themes/due-processed/functions.php
+++ wp-content/themes/due-processed/functions.php
@@ -218,3 +218,8 @@ if ( defined( 'JETPACK__VERSION' ) ) {
 if ( class_exists( 'WooCommerce' ) ) {
 	require get_template_directory() . '/inc/woocommerce.php';
 }
+
+/**
+ * Extra profile fields
+ */
+require get_template_directory() . '/inc/profile-fields.php';
