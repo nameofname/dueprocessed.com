592d1fb383b63b8a624ddba4e723c315fd1563a0
Liliana Matos <liliana@1stdibs.com>
2021-03-16 19:52:15 -0400

    input and about page uat tweaks (#70)
    * hide system default input cancel button
    
    * custom-paragraph uat tweaks
    
    * tweak masthead styles and spacing
    
    * use shortcode for author masthead
    
    * add generated styles

--- due-processed/inc/masthead.php
+++ due-processed/inc/masthead.php
@@ -1,8 +1,23 @@
 <?php
+
+//////////////////////////////////////////////////////
+//
+// Globals
+//
+//////////////////////////////////////////////////////
+
+$DP_MASTHEAD_SHORTCODES = array('due_processed_author_masthead');
+
+//////////////////////////////////////////////////////
+//
+// Carbon Fields
+//
+//////////////////////////////////////////////////////
+
 use Carbon_Fields\Container;
 use Carbon_Fields\Field;
 
-function crb_attach_post_meta() {
+function due_processed_masthead_crb_attach_post_meta() {
 	$post = get_page_by_path( '/about/', OBJECT, 'page');
 	$id = $post ? $post->ID : null;
 	Container::make( 'post_meta', __( 'Page Options' ) )
@@ -17,4 +32,68 @@ function crb_attach_post_meta() {
 				) ),
 		) );
 }
-add_action( 'carbon_fields_register_fields', 'crb_attach_post_meta' );
+add_action( 'carbon_fields_register_fields', 'due_processed_masthead_crb_attach_post_meta' );
+
+//////////////////////////////////////////////////////
+//
+// Masthead Markup
+//
+//////////////////////////////////////////////////////
+
+function due_processed_masthead_func($atts, $content = "") {
+
+	$ret = '';
+	$authors = carbon_get_the_post_meta( 'authors' );
+
+	if ($authors) {
+
+		$ret .= '
+			<div class="masthead-container">
+				<h2 class="masthead-header">Authors</h2>
+				<div class="masthead">
+		';
+
+		foreach ($authors as $author) {
+			$ret.= '
+				<div class="masthead-item">' .
+					wp_get_attachment_image( $author["image"], "thumbnail", false, array("alt" => "Author avatar", "class" => "masthead-image")) .
+					'<div class="masthead-text">
+						<p class="masthead-title">' . $author["title"] . '</p>
+						<p class="masthead-name">' . $author["name"] . '</p>
+					</div>
+				</div>
+			';
+		}
+
+		$ret .= '
+				</div>
+			</div>
+		';
+	}
+	return $ret;
+}
+
+foreach ($DP_MASTHEAD_SHORTCODES as $shortcode) {
+  add_shortcode('due_processed_author_masthead', 'due_processed_masthead_func');
+}
+
+//////////////////////////////////////////////////////
+//
+// RSS Feeds
+//
+//////////////////////////////////////////////////////
+
+function due_processed_masthead_return_blank_for_rss_func($atts, $content = "") {
+  return "";
+}
+
+function due_processed_masthead_remove_from_rss_feed($content) {
+  foreach ($GLOBALS['DP_MASTHEAD_SHORTCODES'] as $shortcode) {
+    remove_shortcode($shortcode);
+    add_shortcode($shortcode, 'due_processed_masthead_return_blank_for_rss_func');
+  }
+  return $content;
+}
+
+add_filter('the_excerpt_rss', 'due_processed_masthead_remove_from_rss_feed');
+add_filter('the_content_feed', 'due_processed_masthead_remove_from_rss_feed');
--- due-processed/page-about.php
+++ due-processed/page-about.php
@@ -1,39 +1,13 @@
 <?php
-
 get_header();
 ?>
-
 	<main id="primary" class="site-main">
-
 		<?php
 		while ( have_posts() ) : the_post(); ?>
 			<div class="about-container">
 				<?php get_template_part( 'template-parts/content', 'page' ); ?>
 			</div>
-
-			<?php
-			// Authors
-			$authors = carbon_get_the_post_meta( 'authors' );
-			if( $authors ):?>
-				<div class='masthead-container'>
-					<h1 class='masthead-header'>Authors</h1>
-					<div class='masthead'>
-						<?php foreach( $authors as $author ): ?>
-							<div class='masthead-item'>
-								<?php echo wp_get_attachment_image( $author['image'], 'thumbnail', false, array('alt' => 'Author avatar', 'class' => 'masthead-image')); ?>
-								<div class='masthead-text'>
-									<p class='masthead-title'><?php echo $author['title']; ?></p>
-									<p class='masthead-name'><?php echo $author['name']; ?></p>
-								</div>
-							</div>
-						<?php endforeach; ?>
-					</div>
-				</div>
-			<?php endif;
-
-		endwhile; // End of the loop.
-		?>
-
+		<?php endwhile; ?>
 	</main><!-- #main -->
 <?php
 get_footer();
