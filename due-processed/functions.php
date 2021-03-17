4e4172c6fa6317917e9357859b1758c66e27afe0
nameofname <nameofemail@gmail.com>
2021-03-17 01:08:39 -0400

    added reusable blocks admin page

--- due-processed/functions.php
+++ due-processed/functions.php
@@ -201,6 +201,16 @@ function due_processed_editor_scripts() {
 add_action( 'enqueue_block_editor_assets', 'due_processed_editor_scripts' );
 
 /**
+ * Reusable Blocks accessible in backend
+ * @link https://www.billerickson.net/reusable-blocks-accessible-in-wordpress-admin-area
+ *
+ */
+function be_reusable_blocks_admin_menu() {
+    add_menu_page( 'Reusable Blocks', 'Reusable Blocks', 'edit_posts', 'edit.php?post_type=wp_block', '', 'dashicons-editor-table', 22 );
+}
+add_action( 'admin_menu', 'be_reusable_blocks_admin_menu' );
+
+/**
  * Implement the Custom Header feature.
  */
 require get_template_directory() . '/inc/custom-header.php';
